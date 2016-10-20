39a1714e3ebb5a6c8e4e2006b899865976958218
Shiguang Wang <sgwang@fb.com>
2016-10-20 03:56:12 -0500

    Updated install.h

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f '$HOME/.tmux.conf' ]; then
-  mv $HOME/.tmux.conf $HOME/.tmux.conf.$(date -u)
+if [ -f $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.$(date +%F_%H:%M)
 fi
 
-ln $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
